f28bab296efbb65eb03a4fcfccbf16e0debf4f96
Hatice <haticekaraty@gmail.com>
2021-08-10 16:03:37 -0400

    refactor renderWord as an instance method of Word

--- src/word.js
+++ src/word.js
@@ -1,19 +1,27 @@
-function renderWord(str){
-    let wordContainer = document.querySelector("#word-container");
-
-    for(let i=0; i<str.length; i++){
-        let letterContainer = document.createElement("div");
-        if(str[i]=""){
-            letterContainer.innerText ="&nbsp"
-        }
-        else{
-
-            letterContainer.classList.add("letter-container");
-            letterContainer.classList.add("mr-3")
-            letterContainer.innerText =str[i].toUpperCase()
-        }
-        wordContainer.appendChild(letterContainer)
+class Word{
+    constructor(str){
+        this.str = str
     }
-    return wordContainer
+
+    renderWord(){
+        let wordContainer = document.querySelector("#word-container");
+
+        for(let i=0; i<this.str.length; i++){
+            console.log(this.str.length)
+            let letterContainer = document.createElement("div");
+            if(this.str[i]===""){
+                letterContainer.innerText ="&nbsp"
+            }
+            else{
+                letterContainer.classList.add("letter-container");
+                letterContainer.classList.add("mr-3")
+                letterContainer.innerText =this.str[i].toUpperCase()
+            }
+            wordContainer.appendChild(letterContainer)
+        }
+        return wordContainer
+    }
 }
-console.log(renderWord("Helloo  Hatice"))
+// console.log(renderWord("Helloo  Hatice"))
+let word = new Word("Hello Hatice")
+console.log(word.renderWord())
