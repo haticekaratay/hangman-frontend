b968d4e4b036b941601c2fbc72b0e4f6c7db2b74
Hatice <haticekaraty@gmail.com>
2021-08-11 19:55:44 -0400

    disables letter buttons once clicked

--- src/word.js
+++ src/word.js
@@ -6,27 +6,45 @@ class Word{
         this.category = category
         Word.all.push(this)
     }
-  
+
     renderWord(){
         let wordContainer = document.querySelector("#word-container");
-        let categoryContainer = document.createElement("div")
-        categoryContainer.innerText = this.category
-        wordContainer.appendChild(categoryContainer)
+
+        let categoryContainer = document.querySelector("#category-container")
+        
+        categoryContainer.innerText = this.category.toUpperCase()
+        categoryContainer.className = "title"
+        
 
         for(let i=0; i<this.name.length; i++){
-            console.log(this.name.length)
+            
             let letterContainer = document.createElement("div");
             if(this.name[i]===""){
                 letterContainer.innerText ="&nbsp"
             }
             else{
                 letterContainer.classList.add("letter-container","mr-3");
-                letterContainer.innerText =this.name[i].toUpperCase()
+                //letterContainer.innerText ="&nbsp"
+                letterContainer.innerHTML = ` `
+                letterContainer.innerHTML =`${this.name[i].toUpperCase()}`
             }
             wordContainer.appendChild(letterContainer)
         }
-        return wordContainer
+        wordContainer.classList.add("d-flex", "justify-content-center")
+        return this.name
+    }
+
+    letterClick(){
+        let letterButtons = document.querySelectorAll("#button")
+        letterButtons.forEach(button => {
+            button.addEventListener("click", (e)=>
+                    e.target.disabled = true)})
+                
     }
 
 }
+
+        
+
+
 
