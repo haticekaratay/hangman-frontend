cfb57a1363eca8e5229b731e47a3f0e6dd4ffbc6
Hatice <haticekaraty@gmail.com>
2021-08-12 22:39:59 -0400

    finds index of repeating letters

--- src/word.js
+++ src/word.js
@@ -4,10 +4,11 @@ class Word{
         this.name = wordObject.name
         this.id = wordObject.id
         this.category = wordObject.category
-     
+        this.space = this.renderSpace()
+        
     }
    
-    renderWord(){
+    renderSpace(){
         let wordContainer = document.querySelector("#word-container");
 
         let categoryContainer = document.querySelector("#category-container")
@@ -19,39 +20,58 @@ class Word{
         for(let i=0; i<this.name.length; i++){
             
             let letterContainer = document.createElement("div");
-            if(this.name[i]===""){
-                letterContainer.innerText ="&nbsp"
-            }
-            else{
-                letterContainer.classList.add("letter-container","mr-3");
+           
+            wordContainer.classList.add("d-flex", "justify-content-center");
                 //letterContainer.innerText ="&nbsp"
-                letterContainer.innerHTML = ` `
-                letterContainer.innerHTML =`${this.name[i].toUpperCase()}`
-            }
-            wordContainer.appendChild(letterContainer)
+                if(this.name[i]===" "){
+                    // letterContainer.classList.add("letter-container","mr-3")
+                    //letterContainer.innerText =this.name[i]
+                    letterContainer.className = "empty-space"
+                }else{
+                    letterContainer.classList.add("letter-container","mr-3")
+                    //letterContainer.innerText =this.name[i].toUpperCase()
+                    letterContainer.innerText =" "
+                }
+                //letterContainer.style.display = "block"
+                console.log(this.name)
+                wordContainer.appendChild(letterContainer)
         }
-        wordContainer.classList.add("d-flex", "justify-content-center")
-        
-        return this.name
-    }
+            
+            return this.name
+        }
 
+    findIndex(word,letter){
+
+        let repeatingLetterIndex =[]
+        
+        word.toLowerCase().split("").filter((l,index)=>{
+            if(l===letter.toLowerCase()){
+               repeatingLetterIndex.push(index)
+            }
+        })
+        console.log("repeating letters", repeatingLetterIndex)
+        return repeatingLetterIndex;
+    }
+            
+    
     letterClick(){
         let letterButtons = document.querySelectorAll("#button")
         letterButtons.forEach(button => {
             button.addEventListener("click", (e)=>{
                     e.target.disabled = true
-                    console.log(this.name)
-             } )})
-                
+                    let value = e.target.value
+                    const indexArray = this.findIndex(this.name,value)
+                    console.log(indexArray)
+                    this.displayLetter(indexArray,value)
+
+            })})     
     }
 
-    isMatched(letter){
-        let letterButtons = document.querySelectorAll("#button")
-        letterButtons.forEach(button=> {
-           if(button.value === letter){
+    isMatched(letter, value){
+        
+        if(value === letter){
                return true
            }
-        })
         return false
     }
 
