594813ebd48d30b99d5c12f1e4bea47a6c15c97e
Hatice <haticekaraty@gmail.com>
2021-08-18 22:47:14 -0400

    get user input from welcome modal to createPlayer

--- src/game.js
+++ src/game.js
@@ -1,9 +1,36 @@
 class Game{
-    constructor({player_id,isFinished,score}){
+
+    constructor({player_id, score}){
         this.player_id = player_id
-        this.isFinished = isFinished
         this.score = score
         this.word = new Word()
     }
+
+    static displayWelcome(){
+        let modalTitle = document.querySelector("#modalTitle")
+        let modalBody = document.querySelector("#modalBodyText")
+        let savePlayerButton = document.querySelector("#savePlayer")
+        let closeButton = document.querySelector("#close")
+        modalTitle.innerText = "Welcome to our Game of Hangman, Choose your letters wisely, you have 6 lives for each word. If you guess a word correctly, you will regain your lives and guess the next word"
+        let input = document.createElement("input")
+    
+        modalBody.innerText = "Please enter your name: "
+        
+        input.type = "text"
+        input.placeholder ="Your Name"
+        modalBody.append(input)
+     
+        $("#gameEnd").modal("show");
+
+        savePlayerButton.addEventListener("click",(e)=>{
+           e.preventDefault()
+           if(input.value.length>0){
+               PlayerAPI.createPlayer({name: input.value});
+            }
+         
+        })
+    }
+
+  
 
 }
